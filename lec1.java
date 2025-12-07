ff52435a117b1580f13593c98a3a524b6e445c5b
shiva <shivachouksey2107@gmail.com>
2025-12-07 18:26:18 +0530

    initial commit

---
+++ lec1.java
@@ -0,0 +1,11 @@
+// import java.util.Scanner;
+
+public class lec1 {
+
+    public static void main(String[] args) {
+        StringBuilder s1 = new StringBuilder("Hello");
+        // s1.insert(1, "bye");
+        // s1.append(false);
+        System.out.println(s1);
+    }
+}
