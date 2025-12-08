f44493af4bd897311dd21c50ce4ac13a9160f443
shiva <shivachouksey2107@gmail.com>
2025-12-08 19:57:51 +0530

    commit

--- lec2.java
+++ lec2.java
@@ -1,8 +1,8 @@
 
-// SumArray
-// import java.util.Scanner;
-// public class lec2 {
+//Sum Array
 
+//  import java.util.Scanner;
+//  public class lec2 {
 //    public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         System.out.print("Enter size of array");
