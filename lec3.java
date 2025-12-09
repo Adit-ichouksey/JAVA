677b2cc750d6a18f6ca1ecf5f3827e358c1e7517
shiva <shivachouksey2107@gmail.com>
2025-12-10 05:29:52 +0530

    Basic construct

---
+++ lec3.java
@@ -0,0 +1,77 @@
+
+// public class lec3 {
+
+ //   public static void main(String[] args) {
+
+        // StringBuilder s1 = new StringBuilder("Hello");
+        // s1.insert(1, "bye");
+        // s1.append(false);
+        // System.out.println(s1);
+        //....
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter value of a");
+        // int a = sc.nextInt();
+        // System.out.print("Enter value of b");
+        // int b = sc.nextInt();
+        // System.out.println(a);
+        //   // System.out.println(b);
+        //....
+        // int n = 123;
+        // byte a = 5;
+        // byte b = 6;
+        // byte c = (byte) (a + b);
+        // System.out.println(c);
+        //....
+        //  int a = 10;
+        // short b = (short) a;
+        // System.out.println(b);
+        // //short a = 5;
+        // // byte b = (byte) a;
+        // //System.out.println(b);
+        //....
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println(i);
+        // }
+        //....
+        // // int a = 8;
+        // int a = 2;
+        // if (a % 2 == 0) {
+        //     System.out.println("Even");
+        // } else {
+        //     System.out.println("Odd");
+        // }
+        //....
+        // byte a = (byte) 130;
+        // short b = 5;
+        // int c = 5;
+        // float f = 90.3f;
+        // boolean data = false;
+        // System.out.println(b);
+        // System.out.println(a);
+        // // java => JDK=> JVM,JRE
+        // // hello.java=>
+        //....
+        // int a = 5;
+        // int b = 10;
+        // if (a < 4 && b == 20) {
+        //     System.out.println("even");
+        // }
+        // System.out.println("odd");
+        //....
+        // int a = 5;
+        // int b = 10;
+        // System.out.println(a + b);
+        // if (a % 2 == 0) {
+        //     System.out.println("even");
+        // } else {
+        //     System.out.println("odd");
+        // }
+        //....
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+   // }
+// }
