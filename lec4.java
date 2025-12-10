49d27706e398a1ff4999373278c48299edb8c6bb
shiva <shivachouksey2107@gmail.com>
2025-12-10 06:37:29 +0530

    input from user

---
+++ lec4.java
@@ -0,0 +1,65 @@
+
+// public class lec4 {
+
+    // public static void main(String[] args) {
+        // String str = "madam";
+        // int count = 0;
+        // for (int i = 0; i < str.length() / 2; i++) {
+        //     if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+        //         count++;
+        //         break;
+        //     }
+        //     if (count == 0) {
+        //         System.out.println("Palindrome");
+        //     } else {
+        //         System.out.println("Not Palindrome");
+        //     }
+        // }
+        //..
+
+        // String str = "['h','e','l']";
+        // for (int i = 0; i < str.length(); i++) {
+        //     System.out.println(str.charAt(i));
+        // }
+        // System.out.println(str.charAt(str.length() - 1));
+        // str = str.concat("hii");
+        // System.out.println(str);
+        //..
+
+        // int arr[] = {1, 2, 4567, 45};
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
+        // }
+        // System.out.println(arr.length);
+        //..
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter you int value");
+        // int a = sc.nextInt();
+        // System.out.println("Enter you float value");
+        // float b = sc.nextFloat();
+        // System.out.print("Enter single word: ");
+        // String c = sc.next();  // single word
+        // sc.nextLine(); // buffer clear karne ke liye
+        // System.out.print("Enter full line: ");
+        // String d = sc.nextLine();  // full line input
+        // System.out.println("Integer: " + a);
+        // System.out.println("Double: " + b);
+        // System.out.println("Word: " + c);
+        // System.out.println("Line: " + d);
+        //..
+
+        // String s = "Hello";
+        // System.out.println(s.indexOf("H"));
+        // for (int i = 0; i < s.length(); i++) {
+        //     System.out.print(s.charAt(i));
+        // }
+        // System.out.println(s.charAt(1));
+        // s = s.concat(" World");
+        // System.out.println(s);  // Output: Hello
+        // String s2 = "hello";
+        // String s1 = new String("hello");
+        // System.out.println(s1 == s2);
+        
+    // }
+// }
