7d2168621aaa4e1f36538621685e7f93b9cb60d0
shiva <shivachouksey2107@gmail.com>
2025-12-12 08:23:44 +0530

    Basic construct

---
+++ lec6.java
@@ -0,0 +1,103 @@
+
+// public class lec6 {
+
+   // public static void main(String[] args) {
+        //  String str="hello";
+        //     String newStr="";
+        //     for(char ch : str.toCharArray()){
+        //         if(newStr.indexOf(ch)==-1){
+        //             newStr+=ch;
+        //         }
+        //     }
+        //     System.out.println(newStr);
+        //
+        //
+        // freq check of each char
+        // int freq[] = new int[256];
+        // String str = "banana";
+        // for (char ch : str.toCharArray()) {
+        //     freq[ch]++;
+        // }
+        // for (int i = 0; i < 256; i++) {
+        //     if (freq[i] > 0) {
+        //         System.out.println((char) i + "-> " + freq[i]);
+        //     }
+        // }
+        //
+        //
+        // Anagram check
+        // String a = "hello";
+        // String b = "lloeh";
+        // char ch1[] = a.toCharArray();
+        // char ch2[] = b.toCharArray();
+        // Arrays.sort(ch1);
+        // Arrays.sort(ch2);
+        // String str1 = new String(ch1);
+        // String str2 = new String(ch2);
+        // if (str1.equals(str2)) {
+        //     System.out.println(true);
+        // } else {
+        //     System.out.println(false);
+        // }
+        //
+        //
+        // String str = "java is powerful";
+        // String a[] = str.split(" ");
+        // String lagest = "";
+        // for (String val : a) {
+        //     if (val.length() > lagest.length()) {
+        //         lagest = val;
+        //     }
+        // }
+        // System.out.println(lagest);
+        //
+        //
+        // String str = "hello";
+        // String str1 = "hlloe";
+        // char ch[] = str.toCharArray();
+        // char ch1[] = str1.toCharArray();
+        // Arrays.sort(ch);
+        // Arrays.sort(ch1);
+        // String val1 = new String(ch);
+        // String val2 = new String(ch1);
+        // if (val1.equals(val2)) {
+        //     System.out.println(true);
+        // } else {
+        //     System.out.println(false);
+        // }
+        //
+        //
+        // int freq[] = new int[256];
+        // String str = "hello world";
+        // String newstr = " Some output";
+        // for (char ch : str.toCharArray()) {
+        //     freq[ch]++;
+        // }
+        // for (int i = 0; i < 256; i++) {
+        //     if (freq[i] > 0) {
+        //         System.out.println((char) i + "->" + freq[i]);
+        //         System.out.println(newstr);
+        //     }
+        // }
+        //
+        //
+        // String str = "hello is powerfull";//['h','e','l','l','o']
+        // String a[] = str.split(" ");
+        // String largest = "";
+        // for (String val : a) {
+        //     if (val.length() > largest.length()) {
+        //         largest = val;
+        //     }
+        // }
+        // System.out.println(largest);
+        // int v = 0;
+        // for (char ch : str.toCharArray()) {
+        //     if ("aeuio".indexOf(ch) != -1) {
+        //         v++;
+        //     }
+        // }
+        // System.out.println(v);
+
+   // }
+
+// }
